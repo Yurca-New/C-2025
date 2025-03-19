5bdfcc1ee4984b8b4510cb4e073273ebb14d33a7
Yurca-New <nazaruk.yurii@student.ehu.lt>
2025-03-19 21:03:43 +0200

    qq

--- Lab1/Task2/Program.cs
+++ Lab1/Task2/Program.cs
@@ -1,4 +1,5 @@
-﻿using Task2;
+﻿using System;
+using Task2;
 List<SportsMan> sportsmans = new List<SportsMan>();
 
 Console.WriteLine("");
@@ -14,3 +15,53 @@ for (int i = 0; i < 1;)
     }
 }
 Console.WriteLine(coutnSportsmans);
+for (int i = 0; i < coutnSportsmans; i++)
+{
+    Console.WriteLine($"Ввудите фамилию спорцмена {i + 1}");
+    string surname = Console.ReadLine();
+    int birthYear = 0;
+    for (int j = 0; j < 1;)
+    {
+        Console.WriteLine($"Ведите год рождения спорцмена {i + 1}");
+        if (int.TryParse(Console.ReadLine(), out birthYear) && birthYear > 0)
+        {
+            j++;
+        }
+        else
+        {
+            Console.WriteLine("ГОД РОЖДЕНИЯ ЭТО ТОЛЬКО ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО");
+        }
+    }
+    int countResults = 0;
+    for (int j = 0; j < 1;)
+    {
+        Console.WriteLine($"Введите количество результатов спорцмена {i + 1}");
+        if (int.TryParse(Console.ReadLine(), out countResults) && countResults > 0)
+        {
+            j++;
+        }
+        else
+        {
+            Console.WriteLine("КОЛИЧЕСТВО ЭТО ТОЛЬКО ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО");
+        }
+    }
+    double[] result = new double[countResults];
+    for (int j = 0; j < countResults; j++)
+    {
+        for (int z = 0; z < 1;)
+        {
+            Console.WriteLine($"Введите {j + 1} результат спорцмена {i + 1}");
+            if (double.TryParse(Console.ReadLine(), out result[j]) && coutnSportsmans > 0)
+            {
+                z++;
+            }
+            else
+            {
+                Console.WriteLine("КОЛИЧЕСТВО ЭТО ТОЛЬКО ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ ЧИСЛО");
+            }
+        }
+    }
+    Console.WriteLine(surname);
+    Console.WriteLine(birthYear);
+    Console.WriteLine(result);
+}
--- Lab1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Lab1/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9553388ca3744238237bb7a651017590811e9d25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f46459a063bf023eb876f264bdf82c09a5b76c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
