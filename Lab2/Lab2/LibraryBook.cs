73638c68a50c7343692a7bfaa7d3df6fefe4638f
Yurca-New <nazaruk.yurii@student.ehu.lt>
2025-04-01 14:05:35 +0300

    Make Lab2

---
+++ Lab2/Lab2/LibraryBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class LibraryBook
+    {
+        private readonly UserInterface _userInterface = new UserInterface();
+        
+        private readonly string title;
+        private readonly string author;
+        private bool isAvailable;
+
+        public string Title => title;
+        public string Author => author;
+        public bool IsAvailable => isAvailable;
+
+        public LibraryBook(string title, string author)
+        {
+            this.title = _userInterface.TestNullString(title, "The title of the book cannot be empty");
+            this.author = _userInterface.TestNullString(author, "The author of the book cannot be empty");
+            this.isAvailable = true;
+        }
+        public void BorrowBook()
+        {
+            if (this.isAvailable)
+                this.isAvailable = false;
+            else
+                _userInterface.ShowMessage("The book is not available");
+        }
+        public void ReturnBook() =>
+            this.isAvailable = true;
+
+        public string GetInfo()
+        {
+            return $"Title: {this.title}, Author: {this.author}, Available: {this.isAvailable}";
+        }
+    }
+}
---
+++ Lab2/Lab2/UserInterface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    internal class UserInterface
+    {
+        public void ShowMessage(string message) =>
+            Console.WriteLine(message);
+
+        public void ShowError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Error: {message}");
+            Console.ResetColor();
+        }
+        public string GetNonNullString(string prompt)
+        {
+            while (true) 
+            {
+                Console.WriteLine(prompt);
+                string result = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(result))
+                    return result;
+                
+                else
+                    ShowError("The entered text must not be empty");
+            }   
+        }
+        public string TestNullString(string count, string prompt) 
+        {
+            if (string.IsNullOrWhiteSpace(count))
+            {
+                ShowError(prompt);
+                Environment.Exit(0);
+                return "..";
+            }
+            else
+                return count;
+        }
+    }
+}
