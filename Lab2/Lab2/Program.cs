22d3487c51d88a1210c2b3e4f038c006f07b8f4e
Yurca-New <nazaruk.yurii@student.ehu.lt>
2025-04-08 12:42:56 +0300

    qqwwqq

--- Lab2/Lab2/Program.cs
+++ Lab2/Lab2/Program.cs
@@ -1,1 +1,14 @@
 ﻿using Lab2;
+UserInterface userInterface = new UserInterface();
+LibraryBook book = new LibraryBook("1984", "George Orwell");
+
+userInterface.ShowMessage(book.GetInfo());
+book.BorrowBook();
+userInterface.ShowMessage("After borrowing the book:");
+userInterface.ShowMessage(book.GetInfo());
+
+book.BorrowBook();
+
+book.ReturnBook();
+userInterface.ShowMessage("After returning the book:");
+userInterface.ShowMessage(book.GetInfo());
