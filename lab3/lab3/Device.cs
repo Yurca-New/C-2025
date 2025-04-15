497d20d8f7c5cb06105a9a0dff118e09cf733963
Yurca-New <nazaruk.yurii@student.ehu.lt>
2025-04-15 13:47:40 +0300

    qq

---
+++ lab3/lab3/Device.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public abstract class Device
+    {
+        public abstract void TurnOn();
+    }
+
+    public class Laptop : Device
+    {
+        public override void TurnOn()
+        {
+            Console.WriteLine("Laptop TurnOn");
+        }
+    }
+
+    public class Smartphone : Device
+    {
+        public override void TurnOn()
+        {
+            Console.WriteLine("Smartphone TurnOn");
+        }
+    }
+
+    public class Tablet : Device
+    {
+        public override void TurnOn()
+        {
+            Console.WriteLine("Tablet TurnOn");
+        }
+    }
+}
+        
---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,12 @@
+﻿using lab3;
+Device[] devices = new Device[]
+    {
+        new Laptop(),
+        new Smartphone(),
+        new Tablet()
+    };
+
+foreach (Device device in devices)
+{
+    device.TurnOn();
+}
